efc00e1ee6e4f5bf4444b24d4e7209697a289e1c
dhermansen <danielwhermansen@gmail.com>
2017-03-20 17:11:23 -0400

    Dragging and shaking has strange effects

--- Assets/softbodyinfo.cs
+++ Assets/softbodyinfo.cs
@@ -8,6 +8,7 @@ public class softbodyinfo : FlexProcessor {
     GameObject renal;
     Plane plane;
     bool has_cut = false;
+    List<Color> colors = new List<Color>();
 	// Use this for initialization
 	void Start () {
         renal = GameObject.Find("RenalSystemColor");
@@ -18,18 +19,19 @@ public class softbodyinfo : FlexProcessor {
     // Update is called once per frame
     public override void PostContainerUpdate(FlexSolver solver, FlexContainer cntr, FlexParameters parameters)
     {
+
         if (!has_cut)
         {
             has_cut = true;
-            var fp = renal.GetComponent<FlexParticles>();
-            //CuttingUtil.CutFlexSoft(renal.transform, plane);
+            //colors = CuttingUtil.CutFlexSoft(renal.transform, plane);
         }
+
 
     }
 
     private void OnDrawGizmos()
     {
-        return;
+        //return;
         var fp = renal.GetComponent<FlexParticles>();
         var fsm = renal.GetComponent<FlexShapeMatching>();
         int shapeStart = 0;
@@ -38,24 +40,40 @@ public class softbodyinfo : FlexProcessor {
             int shapeEnd = fsm.m_shapeOffsets[i];
             var actual_ctr = CuttingUtil.shape_to_world(i, fsm);
             bool is_split = false;
+            var ul = new Vector3?();
+            var lr = new Vector3 ? ();
             for (var j = shapeStart; j < shapeEnd; ++j)
             {
                 var idx = fsm.m_shapeIndices[j];
-                if (!plane.SameSide(actual_ctr, fp.m_particles[idx].pos))
-                {
-                    //Gizmos.color = Color.green;
-                    //Gizmos.DrawCube(fp.m_particles[idx].pos, new Vector3(0.5f, 0.5f, 0.5f));
-                    is_split = true;
-                }
+                var pos = fp.m_particles[idx].pos;
+                ul = ul.HasValue ? Vector3.Min(ul.Value, pos) : pos;
+                lr = lr.HasValue ? Vector3.Max(lr.Value, pos) : pos;
             }
-            if (is_split)
-            {
-                Gizmos.color = Color.blue;
-                Gizmos.DrawCube(actual_ctr, new Vector3(0.5f, 0.5f, 0.5f));
-            }
+            Gizmos.color = colors[i];
+
+            var shape_center = (ul.Value + lr.Value) / 2.0f;
+            Gizmos.DrawCube(shape_center, lr.Value - ul.Value);
+     
             shapeStart = shapeEnd;
         }
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(renal.transform.position, new Vector3(0.1f, 20.0f, 20.0f));
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawCube(renal.transform.position, new Vector3(0.1f, 20.0f, 20.0f));
+        //for (int i = 0; i < fp.m_particlesCount; ++i)
+        //{
+        //    if (colors.Count > 0)
+        //        Gizmos.color = colors[i];
+        //    Gizmos.DrawCube(fp.m_particles[i].pos, new Vector3(0.2f, 0.2f, 0.2f));
+        //}
+        //var skin = renal.GetComponent<SkinnedMeshRenderer>();
+        //var mesh = new Mesh();
+        //skin.BakeMesh(mesh);
+        //var mesh = skin.sharedMesh;
+        //for (int i = 0; i < mesh.vertexCount; ++i)
+        //{
+        //    var pos = renal.transform.rotation * mesh.vertices[i] + renal.transform.position;
+        //    if (colors.Count != 0)
+        //        Gizmos.color = colors[i];
+        //    Gizmos.DrawCube(pos, new Vector3(0.2f, 0.2f, 0.2f));
+        //}
     }
 }
--- Assets/uFlex/Scripts/CuttingUtil.cs
+++ Assets/uFlex/Scripts/CuttingUtil.cs
@@ -26,7 +26,7 @@ public class CuttingUtil
         int particle_idx;
     }
 
-    public static void CutFlexSoft(Transform target, Plane act_plane)
+    public static List<Color> CutFlexSoft(Transform target, Plane act_plane)
     {
         var fsm = target.GetComponent<FlexShapeMatching>();
         var fp = target.GetComponent<FlexParticles>();
@@ -38,21 +38,21 @@ public class CuttingUtil
 
         //Stores offsets of each shape in the indices array.  IOW we can determine which indices are split from each shape by plane
         var offsets = new List<int>();
-
+        var centers = fsm.m_shapeCenters.Select((pos,idx) => shape_to_world(idx, fsm)).ToList();
         int shape_start = 0;
         //For each shape, determine which indices are split from each shape by plane.
         for (int shape_idx = 0; shape_idx < fsm.m_shapesCount; ++shape_idx)
         {
             int shape_end = fsm.m_shapeOffsets[shape_idx];
 
-            var shape_center = shape_to_world(shape_idx, fsm);
+            var shape_center = centers[shape_idx];
             for (int j = shape_start; j < shape_end; ++j)
             {
                 var particle_idx = fsm.m_shapeIndices[j];
-                if (act_plane.SameSide(shape_center, fp.m_particles[particle_idx].pos))
-                    correct_side_indicies.Add(j);
-                else
-                    wrong_side_indices.Add(j);
+                //if (act_plane.SameSide(shape_center, fp.m_particles[particle_idx].pos))
+                    correct_side_indicies.Add(particle_idx);
+                //else
+                    //wrong_side_indices.Add(particle_idx);
             }
             offsets.Add(correct_side_indicies.Count);
             shape_start = shape_end;
@@ -64,95 +64,128 @@ public class CuttingUtil
             if (!correct_side_indicies.Contains(idx))
             {
                 int shape_index = find_closest_shape_on_same_side(
-                    fsm.m_shapeCenters, fp.m_restParticles[idx].pos, act_plane);
+                    centers, fp.m_particles[idx].pos, act_plane);
                 int new_shape_start_index = offsets[shape_index];
                 correct_side_indicies.Insert(new_shape_start_index, idx);
-                for (int j = shape_index; j < offsets.Count; j++)
+                for (int j = shape_index; j < offsets.Count; ++j)
                     offsets[j] += 1;
             }
-
         }
 
-        //Now, store the modified shapes back into the flex object.
+
+        ////Now, store the modified shapes back into the flex object.
         fsm.m_shapeIndicesCount = correct_side_indicies.Count;
         fsm.m_shapeIndices = correct_side_indicies.ToArray();
         fsm.m_shapeOffsets = offsets.ToArray();
 
-//        //Calculate centers for the newly reconfigured shapes
-//        int shape_start = 0;
-//        int shapeIndexOffset = 0;
-//        for (int s = 0; s < fsm.m_shapesCount; s++)
-//        {
-//            //shapes.m_shapeTranslations[s] = new Vector3();
-//            //shapes.m_shapeRotations[s] = Quaternion.identity;
+        ////Calculate centers for the newly reconfigured shapes
+        shape_start = 0;
+        int shape_index_offset = 0;
+        for (int s = 0; s < fsm.m_shapesCount; ++s)
+        {
+            fsm.m_shapeTranslations[s] = new Vector3();
+            fsm.m_shapeRotations[s] = Quaternion.identity;
 
-//            int shapeEnd = fsm.m_shapeOffsets[s];
+            int shape_end = fsm.m_shapeOffsets[s];
 
-//            //For each particle in shape, calculate a new shape center.
-//            int num_shapes = shapeEnd - shape_start;
-//            fsm.m_shapeCenters[s] = fsm.m_shapeIndices.ToList().GetRange(shape_start, num_shapes)
-//                .Aggregate(Vector3.zero, (lhs, p_idx) => lhs + fp.m_restParticles[p_idx].pos) / num_shapes;
-//            //And adjust other particles in shape to be relative to the new center.
-//            var shape_offsets = fsm.m_shapeIndices.ToList().GetRange(shape_start, num_shapes)
-//                .Select(p_idx => {
-//                    fsm.m_shapeRestPositions[shapeIndexOffset] = fp.m_restParticles[p_idx].pos - fsm.m_shapeCenters[s];
-//                    return ++shapeIndexOffset;
-//                });
+            //For each particle in shape, calculate a new shape center.
+            //var shift = act_plane.GetSide(fsm.m_shapeCenters[s]) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0);
+            var shift = new Vector3(0, 0, 0);
+            int num_shapes = shape_end - shape_start;
+            fsm.m_shapeCenters[s] = fsm.m_shapeIndices.ToList().GetRange(shape_start, num_shapes)
+                .Aggregate(Vector3.zero, (lhs, p_idx) => lhs + fp.m_particles[p_idx].pos) / num_shapes + shift;
 
-//            shape_start = shapeEnd;
-//        }
+            //And adjust other particles in shape to be relative to the new center.
+            var shape_offsets = fsm.m_shapeIndices.ToList().GetRange(shape_start, num_shapes)
+                .Select(p_idx =>
+                {
+                    fsm.m_shapeRestPositions[shape_index_offset] = fp.m_restParticles[p_idx].pos - fsm.m_shapeCenters[s] + shift;
+                    return ++shape_index_offset;
+                });
 
-//        //And make sure that the mesh vertices aren't influenced by shapes that are cut off.
-//        //Mesh mesh = target.GetComponent<SkinnedMeshRenderer>().sharedMesh;
-//        //var boneWeights = new BoneWeight[mesh.vertexCount];
-//        //for (int i = 0; i < mesh.vertexCount; i++)
-//        //{
-//        //    mesh.boneWeights[i] = adjust_weight(mesh.boneWeights[i], mesh.vertices[i], shapes.m_shapeCenters, plane);
-//        //}
+            shape_start = shape_end;
+        }
+        //for (int shape_idx = 0; shape_idx < fsm.m_shapesCount; ++shape_idx)
+        //{
+        //    int shape_end = fsm.m_shapeOffsets[shape_idx];
+
+        //    var shape_center = shape_to_world(shape_idx, fsm);
+        //    for (int j = shape_start; j < shape_end; ++j)
+        //    {
+        //        var particle_idx = fsm.m_shapeIndices[j];
+        //        Debug.Assert(act_plane.SameSide(shape_center, fp.m_particles[particle_idx].pos));
+        //    }
+        //}
+        ////And make sure that the mesh vertices aren't influenced by shapes that are cut off.
+        //var skin = target.GetComponent<SkinnedMeshRenderer>();
+        //var mesh = skin.sharedMesh;
+        //var baked = new Mesh();
+        //skin.BakeMesh(baked);
+        //var bone_weights = new BoneWeight[mesh.vertexCount];
+        //for (int i = 0; i < mesh.vertexCount; i++)
+        //{
+        //    bone_weights[i] = mesh.boneWeights[i];
+        //    var pos = target.rotation * baked.vertices[i] + target.position;
+        //    mesh.boneWeights[i] = adjust_weight(bone_weights[i], pos, centers, act_plane);
+        //}
+        var colors = new List<Color>();
+        for (int i = 0; i < centers.Count; ++i)
+        {
+            colors.Add(act_plane.GetSide(centers[i]) ? Color.magenta : Color.cyan);
+        }
+
+        return colors;
     }
-    public static BoneWeight adjust_weight(BoneWeight weight, Vector3 mesh_vert, Vector3[] bones, Plane plane)
+    public static BoneWeight adjust_weight(BoneWeight weight, Vector3 mesh_vert, List<Vector3> particles, Plane plane)
     {
         //return weight;
-        var particles = new List<Vector3> { bones[weight.boneIndex0], bones[weight.boneIndex1],
-                                            bones[weight.boneIndex2], bones[weight.boneIndex3] };
+        var weighted_particles = new List<Vector3> { particles[weight.boneIndex0], particles[weight.boneIndex1],
+                                                     particles[weight.boneIndex2], particles[weight.boneIndex3] };
         var same_side_verts = particles.Select(v => plane.SameSide(v, mesh_vert)).ToList();
-        if (same_side_verts.Where(v=>v).Count() == 0)
+        if (same_side_verts.Where(v => v).Count() == 0)
         {
-            weight.boneIndex0 = find_closest_shape_on_same_side(bones, mesh_vert, plane);
+            weight.boneIndex0 = find_closest_shape_on_same_side(particles, mesh_vert, plane);
             weight.weight0 = 1.0f;
-            weight.weight1 = weight.weight2 = weight.weight3 = 0;
+            weight.weight1 = weight.weight2 = weight.weight3 = 0.0f;
         }
         else
         {
-            var best_idx = find_closest_shape_on_same_side(particles.ToArray(), mesh_vert, plane);
-            float bad_vals = 0;
+            float reallocated_weight = 0.0f;
             if (!same_side_verts[0])
             {
-                bad_vals += weight.weight0;
+                reallocated_weight += weight.weight0;
                 weight.weight0 = 0;
             }
             if (!same_side_verts[1])
             {
-                bad_vals += weight.weight1;
+                reallocated_weight += weight.weight1;
                 weight.weight1 = 0;
             }
             if (!same_side_verts[2])
             {
-                bad_vals += weight.weight2;
-                weight.weight2 = 3;
+                reallocated_weight += weight.weight2;
+                weight.weight2 = 0;
             }
-            if (!same_side_verts[0])
+            if (!same_side_verts[3])
             {
-                bad_vals += weight.weight3;
+                reallocated_weight += weight.weight3;
                 weight.weight3 = 0;
             }
+            reallocated_weight = Mathf.Min(reallocated_weight, 1.0f);
+            if (reallocated_weight > 1.0f+1e-6f)
+                Debug.Log("Updated weighting: " + reallocated_weight.ToString("F4"));
+            var best_idx = find_closest_shape_on_same_side(weighted_particles, mesh_vert, plane);
+            if (best_idx == 0) weight.weight0 += reallocated_weight;
+            if (best_idx == 1) weight.weight1 += reallocated_weight;
+            if (best_idx == 2) weight.weight2 += reallocated_weight;
+            if (best_idx == 3) weight.weight3 += reallocated_weight;
         }
         return weight;
     }
 
-    public static int find_closest_shape_on_same_side(Vector3[] bonePos, Vector3 vert, Plane plane)
+    public static int find_closest_shape_on_same_side(List<Vector3> particle, Vector3 vert, Plane plane)
     {
-        return bonePos.ToList().Where((pos, idx) => plane.SameSide(pos, vert))
+        return particle.ToList().Where((pos, idx) => plane.SameSide(pos, vert))
             .Select((pos, idx) => new { idx, dist = Vector3.Distance(pos, vert) })
             .Aggregate(new { idx = -1, dist = float.MaxValue }, (lhs, rhs) => lhs.dist < rhs.dist ? lhs : rhs).idx;
     }
